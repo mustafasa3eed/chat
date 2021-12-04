12af52d17b0c517f58fb2a639a78fecf9d81e4c9
bugs <mustafasa3eed98@gmail.com>
2021-12-04 16:16:30 +0200

    firebase auth is connected

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -25,106 +25,111 @@ class RegisterScreen extends StatelessWidget {
           elevation: 0,
           title: Text('SignUp'),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Form(
-              key: formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.person),
-                        labelText: 'First Name'),
-                    onChanged: (text) {
-                      firstName = text;
-                    },
-                    validator: (text) {
-                      if (text == null || text.trim().isEmpty) {
-                        return 'Please enter your First Name';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.person), labelText: 'Last Name'),
-                    onChanged: (text) {
-                      lastName = text;
-                    },
-                    validator: (text) {
-                      if (text == null || text.trim().isEmpty) {
-                        return 'Please enter your Last Name';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.alternate_email_outlined),
-                      labelText: 'Username',
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height *0.25,
                     ),
-                    onChanged: (text) {
-                      userName = text;
-                    },
-                    validator: (text) {
-                      if (text == null || text.trim().isEmpty) {
-                        return 'Please create a Username';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.mail), labelText: 'E-mail'),
-                    onChanged: (text) {
-                      email = text;
-                    },
-                    validator: (text) {
-                      if (text == null || text.trim().isEmpty) {
-                        return 'Please enter your  Email';
-                      }
-                      if (!isValidEmail(email)) {
-                        return 'Please enter a valid Email';
-                      }
-                    },
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.password),
-                        labelText: 'Password'),
-                    onChanged: (text) {
-                      password = text;
-                    },
-                    validator: (text) {
-                      if (text == null || text.trim().isEmpty) {
-                        return 'Please enter your Password';
-                      }
-                      if (text.length < 9) {
-                        return "password cant be less than 9 char/num/symbol";
-                      }
-                    },
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        if (formKey.currentState?.validate() == true) {
-                          createAccount();
+                    TextFormField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person),
+                          labelText: 'First Name'),
+                      onChanged: (text) {
+                        firstName = text;
+                      },
+                      validator: (text) {
+                        if (text == null || text.trim().isEmpty) {
+                          return 'Please enter your First Name';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person), labelText: 'Last Name'),
+                      onChanged: (text) {
+                        lastName = text;
+                      },
+                      validator: (text) {
+                        if (text == null || text.trim().isEmpty) {
+                          return 'Please enter your Last Name';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.alternate_email_outlined),
+                        labelText: 'Username',
+                      ),
+                      onChanged: (text) {
+                        userName = text;
+                      },
+                      validator: (text) {
+                        if (text == null || text.trim().isEmpty) {
+                          return 'Please create a Username';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.mail), labelText: 'E-mail'),
+                      onChanged: (text) {
+                        email = text;
+                      },
+                      validator: (text) {
+                        if (text == null || text.trim().isEmpty) {
+                          return 'Please enter your  Email';
+                        }
+                        if (!isValidEmail(email)) {
+                          return 'Please enter a valid Email';
                         }
                       },
-                      child: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('Create account'),
-                            Icon(
-                              Icons.arrow_forward_rounded,
-                            ),
-                          ],
-                        ),
-                      ))
-                ],
-              )),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.password),
+                          labelText: 'Password'),
+                      onChanged: (text) {
+                        password = text;
+                      },
+                      validator: (text) {
+                        if (text == null || text.trim().isEmpty) {
+                          return 'Please enter your Password';
+                        }
+                        if (text.length < 9) {
+                          return "password cant be less than 9 char/num/symbol";
+                        }
+                      },
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          if (formKey.currentState?.validate() == true) {
+                            createAccount();
+                          }
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Create account'),
+                              Icon(
+                                Icons.arrow_forward_rounded,
+                              ),
+                            ],
+                          ),
+                        ))
+                  ],
+                )),
+          ),
         ),
       ),
     );
