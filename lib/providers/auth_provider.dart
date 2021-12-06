e52e0415146aead6beecd49d81887846338d42fd
bugs <mustafasa3eed98@gmail.com>
2021-12-06 22:06:52 +0200

    designed login screen

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:chat/data/user.dart' as AppUser;
+import 'package:chat/data/user.dart' as app_user;
 
 class AuthProvider extends ChangeNotifier{
-  AppUser.User? user = null;
-  updateUser(AppUser.User user){
+  app_user.User? user;
+  updateUser(app_user.User user){
     this.user = user;
     notifyListeners();
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
+      color: Colors.red,
       child: Scaffold(
 
       ),
